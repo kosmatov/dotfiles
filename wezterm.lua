5a2b2374da2a5a96a45b42d7851cf56146b9df30
Konstantin Kosmatov <key@kosmatov.ru>
2022-06-14 11:40:26 +0300

    Clean wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,5 @@
 local wezterm = require 'wezterm';
 return {
-  -- font_dirs = {"Library/Fonts"},
   font = wezterm.font("Monaco"),
   font_size = 13,
   freetype_load_flags = "NO_HINTING",
@@ -9,13 +8,10 @@ return {
   window_background_opacity = 1.0,
   window_padding = {
     left = 8,
-    right = 5,
+    right = 8,
     top = 5,
     bottom = 5,
   },
-  -- text_background_opacity = 0.85,
-  -- window_decorations = "TITLE | RESIZE",
-  -- native_macos_fullscreen_mode = true,
   window_background_image = "sleepwatcher-wezterm/images/sunset-clouds-nature-scenery-thunderstorm-lightning.png",
   initial_cols = 175,
   initial_rows = 87,
