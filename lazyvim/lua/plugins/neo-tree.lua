b53eb70f5936b2d4257f4c205cbb790ec1d22cb9
Konstantin Kosmatov <key@kosmatov.com>
2024-09-29 19:27:33 +0300

    Turn back neotree behavior

--- lazyvim/lua/plugins/neo-tree.lua
+++ lazyvim/lua/plugins/neo-tree.lua
@@ -1,5 +1,23 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ dir = vim.env.HOME, reveal = true })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+    },
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ dir = vim.loop.cwd(), reveal = true })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)",      remap = true },
+  },
   opts = {
     enable_git_status = false,
     filesystem = {
